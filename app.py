9f34f6d8c00c2fa7624f09f776978a2184ab72e0
Peat <philippmadl1122@gmail.com>
2024-03-12 11:03:37 +0100

    Extract secret key from config file
    The app's secret key is now loaded from a dedicated JSON configuration file, improving security and modularity. The secret_key.json file contains the secret key which will replace the previously hard-coded value.

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import json
 
 
 app = Flask(__name__)
-app.secret_key =' '
+
+file_path = "config/secret_key.json"
+with open(file_path, 'r') as file:
+    config_secret_key = json.load(file)
+
+app.secret_key = config_secret_key['secret_key']
 
 @app.route('/')
 def hello_world():  # put application's code here
