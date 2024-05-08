ad43f1173c33256a94a64d790a3ea0d6e432d3da
Peat <philippmadl1122@gmail.com>
2024-05-08 12:35:11 +0200

    Implement user login functionality
    Refactored the `login_page` function in auth.py to handle POST requests and added password verification using a hash. Updated login form in login.html accordingly. In app.py, instantiated Flask's LoginManager and set up a function for loading users.

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g, session, flash, abort
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, MetaData
 from static.get_data_from_json import get_data_from_json
 from db.check_db_connenction import test_db_connection
-from db.models import *
+from flask_login import LoginManager, UserMixin, login_user
 
 
 class Configuration:
-    # The function `__init__` is the constructor of the class. This method is called
-    # when an object of this class is instantiated. Here, it is being used to initialize
-    # various configuration values.
     def __init__(self):
         self.secret_key = get_data_from_json("config/secret_key.json")
         sql_login_data = get_data_from_json("config/db_login.json")
@@ -24,9 +22,16 @@ app.secret_key = config.secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = config.sql_alchemy_database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.sql_alchemy_track_modifications
 
-
 db = SQLAlchemy()
 db.init_app(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = '/'  # Setze die Login-Route
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    pass  # Hier wird die Funktion definiert, aber keine Aktion durchgeführt
 
 
 from controller.auth import auth
@@ -37,7 +42,7 @@ app.register_blueprint(auth, url_prefix="/")
 app.register_blueprint(admin, url_prefix="/admin")
 app.register_blueprint(user, url_prefix="/user")
 
-if __name__ == 'app':
+if __name__ == '__main__':
     with app.app_context():
         test_db_connection(db.engine)
-    app.run()
+    app.run(debug=True)
--- controller/auth.py
+++ controller/auth.py
@@ -1,10 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session, g
+from flask_login import login_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from modell.user_helpers import get_user_by_email
+
+
 
 auth = Blueprint('/', __name__)
 
-@auth.route('/login')
+
+
+
+@auth.route('/', methods=['GET', 'POST'])
 def login_page():
-    return render_template("login.html")
+
+    if request.method == 'POST':
+        #session.pop('username', None)
+        usermail = request.form['email']
+        password = request.form['password']
+
+        #Überprüfe, ob der Benutzername in der Datenbank vorhanden ist
+        user = get_user_by_email(usermail)
+        if user is not None:
+            # Überprüfe das Passwort
+            if check_password_hash(user.password, password):
+               # Logge den Benutzer ein, wenn das Passwort korrekt ist
+               login_user(user)
+               return redirect(url_for('user.index'))
+            else:
+                print("wrong email or password")
+                # Falsches Passwort oder E-Mail
+                return render_template('login.html', error='Ungültige Anmeldeinformationen')
+        else:
+            print("wrong email or password")
+            # Falsches Passwort oder E-Mail
+            return render_template('login.html', error='Ungültige Anmeldeinformationen')
+    else:
+        # Besucherseite ohne eingeloggt zu sein
+        return render_template('login.html')
+
 
 @auth.route('/logout')
 def logout_page():
@@ -13,3 +46,5 @@ def logout_page():
 @auth.route('/Sign-up')
 def signup_page():
     return 0
+
+
